732e5126584dcd75b55544aaf60f11547c55b872
rodrigorieraa <rriera@fi.uba.ar>
2026-09-08 22:26:12 -0300

    [Feat #2]: Final config for neovim.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -43,4 +43,10 @@ opt.completeopt = {
     "noselect",
 }
 
-
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+})
